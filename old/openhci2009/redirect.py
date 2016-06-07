5c3a84e880f2b1dcd093da90e48a72e0f427acfb
Samantha Lin <samanthalin1992@gmail.com>
2016-06-08 02:02:38 +0800

    day1program

--- old/openhci2009/redirect.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-import logging
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-      self.redirect("/2009/Home.html")
-
-app = webapp2.WSGIApplication([('/2009.*', MainPage)],
-                              debug=True)
