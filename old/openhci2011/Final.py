3a87ff1f44be8272dd07ce5f4f41b1098fe4233b
信丞 侯 <dihimono@xinchengdeMacBook-Air.local>
2016-06-08 00:03:55 +0800

    add old websites

---
+++ old/openhci2011/Final.py
@@ -0,0 +1,18 @@
+import os
+import webapp2
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from Constants import debugMode
+
+class FinalPage(webapp2.RequestHandler):
+    def get(self):
+
+        # image, members, cht_summary,
+
+        pagePath = os.path.join(os.path.dirname(__file__), "html/final.html")
+        self.response.out.write(template.render(pagePath, None))
+
+app= webapp2.WSGIApplication([("/final/", FinalPage)], debug=debugMode)
---
+++ old/openhci2011/Passport.py
@@ -0,0 +1,16 @@
+import os 
+import webapp2
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from openhci2011.Constants import debugMode
+
+class PassportPage(webapp2.RequestHandler):
+    def get(self):
+        pagePath = os.path.join(os.path.dirname(__file__), "html/passport.html")
+        pageDict = {}
+        renderResult = template.render(pagePath, pageDict)
+        self.response.out.write(renderResult)
+
+app= webapp2.WSGIApplication([("/2011/passport/", PassportPage)], debug=debugMode)
