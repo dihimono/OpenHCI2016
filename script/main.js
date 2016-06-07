f9b6258071c46a39788cf4986523089fe189d271
Samantha Lin <samanthalin1992@gmail.com>
2016-06-08 05:03:34 +0800

    mute/unmute

--- script/main.js
+++ script/main.js
@@ -49,5 +49,15 @@ $(function(){
   });
 });
 
+$(function(){
+  var audio = document.getElementById('background_audio');
+
+  document.getElementById('mute').addEventListener('click', function (e)
+  {
+      e = e || window.event;
+      audio.muted = !audio.muted;
+      e.preventDefault();
+  }, false);
+});
 
 
