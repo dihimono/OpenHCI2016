e05a7f816356930cdd8c7fbff063060276393a7f
Samantha Lin <samanthalin1992@gmail.com>
2016-07-07 20:05:43 +0800

    aja_update

--- script/main.js
+++ script/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 $(function(){
   // 預設顯示第一個 Tab
-  var _showTab = 1;
+  var _showTab = 2;
   var $defaultLi = $('ul.days li').eq(_showTab).addClass('active');
   $($defaultLi.find('a').attr('href')).siblings().hide();
  
